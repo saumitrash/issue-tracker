0236cba93c54ec965a0acb3cf6057061b876a802
darkyed <darkyed7@gmail.com>
2023-10-18 04:40:41 +0530

    built navbar and added active link styles

---
+++ app/NavBar.tsx
@@ -0,0 +1,38 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
+import classnames from "classnames";
+
+const NavBar = () => {
+  const currentPath = usePathname();
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <ul className="flex space-x-6">
+        <li>
+          <AiFillBug />
+        </li>
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className={classnames({
+              "text-zinc-500": true,
+              "text-zinc-800": currentPath === link.href,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
