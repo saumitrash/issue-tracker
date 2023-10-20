9a97cbdc1892d6db59bdd29d2693fd629bbcd8a4
darkyed <darkyed7@gmail.com>
2023-10-20 16:36:55 +0530

    handle errors

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -3,8 +3,8 @@ import z from "zod";
 import prisma from "@/prisma/client";
 
 const createIssueSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1),
+  title: z.string().min(1, "Title is required").max(255),
+  description: z.string().min(1, "Description is required"),
 });
 
 export async function POST(request: NextRequest) {
@@ -12,7 +12,7 @@ export async function POST(request: NextRequest) {
   const valid = createIssueSchema.safeParse(body);
 
   if (!valid.success) {
-    return NextResponse.json(valid.error.errors, { status: 400 });
+    return NextResponse.json(valid.error.format(), { status: 400 });
   }
 
   const newIssue = await prisma.issue.create({
