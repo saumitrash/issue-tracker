a1a39871c06d85a1c0f1c770bf8e23845fe9e961
darkyed <darkyed7@gmail.com>
2023-10-22 19:17:56 +0530

    added loading skeletons

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,16 +1,12 @@
-import prisma from "@/prisma/client";
-import { Badge, Button, Table, TableColumnHeaderCell } from "@radix-ui/themes";
-import Link from "next/link";
+import { Table } from "@radix-ui/themes";
 import React from "react";
-import IssueStatusBadge from "../components/IssueStatusBadge";
-import { Status } from "@prisma/client";
-import delay from "delay";
-import IssueActions from "./IssueActions";
+import IssueStatusBadge from "./components/IssueStatusBadge";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import IssueActions from "./issues/IssueActions";
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
-  await delay(1000);
-
+const LoadingIssuesPage = () => {
+  const issues = [1, 2, 3, 4];
   return (
     <div>
       <IssueActions />
@@ -26,18 +22,18 @@ const IssuesPage = async () => {
         </Table.Header>
         <Table.Body>
           {issues.map((issue) => (
-            <Table.Row key={issue.id}>
+            <Table.Row key={issue}>
               <Table.RowHeaderCell>
-                {issue.title}
+                <Skeleton />
                 <div className="block md:hidden">
-                  <IssueStatusBadge status={issue.status} />
+                  <Skeleton />
                 </div>
               </Table.RowHeaderCell>
               <Table.Cell className="hidden md:table-cell">
-                <IssueStatusBadge status={issue.status} />
+                <Skeleton />
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
-                {issue.createdAt.toDateString()}
+                <Skeleton />
               </Table.Cell>
             </Table.Row>
           ))}
@@ -47,4 +43,4 @@ const IssuesPage = async () => {
   );
 };
 
-export default IssuesPage;
+export default LoadingIssuesPage;
