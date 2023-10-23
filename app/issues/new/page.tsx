6a462ab9d3fc307895f39c8e5ce1884e4bb90e9a
darkyed <darkyed7@gmail.com>
2023-10-23 19:24:33 +0530

    disabled ssr to load simple mde on client side

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -9,7 +9,12 @@ import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
-import SimpleMDE from "react-simplemde-editor";
+
+import dynamic from "next/dynamic";
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
+
 import { z } from "zod";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
