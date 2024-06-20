34ebbc7fbd18a42391a8d5c7eb7f065469439742
saumitrash <darkyed7@gmail.com>
2024-06-20 05:58:38 +0530

    feat: add drop-down menu avatar for current user

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -6,7 +6,14 @@ import { AiFillBug } from "react-icons/ai";
 import classnames from "classnames";
 // import { getServerSession } from "next-auth";
 import { useSession } from "next-auth/react";
-import { Box, Container, Flex } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
 
 const NavBar = () => {
   const currentPath = usePathname();
@@ -43,7 +50,28 @@ const NavBar = () => {
           </Flex>
           <Box>
             {status === "authenticated" ? (
-              <Link href="/api/auth/signout">Sign out</Link>
+              // <Link href="/api/auth/signout">Sign out</Link>
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <Avatar
+                    src={session.user!.image!}
+                    fallback="?"
+                    size="2"
+                    radius="full"
+                    className="cursor-pointer"
+                  />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                  <Text size="2">
+                    <DropdownMenu.Label>
+                      {session.user?.email}
+                    </DropdownMenu.Label>
+                  </Text>
+                  <DropdownMenu.Item>
+                    <Link href="/api/auth/signout">Sign out</Link>
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
             ) : (
               <Link href="/api/auth/signin">Sign in</Link>
             )}
