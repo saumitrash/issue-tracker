ce65178107083235c35500968012642e703e82c6
saumitrash <darkyed7@gmail.com>
2024-06-21 03:44:43 +0530

    fix: fix filtering issue

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 import React from "react";
 
@@ -13,11 +13,17 @@ const IssueStatusFilter = () => {
     { label: "In Progress", value: "IN_PROGRESS" },
   ];
   const router = useRouter();
+  const searchParams = useSearchParams();
   return (
     <Select.Root
+      defaultValue={searchParams.get("status") || ""}
       onValueChange={(status) => {
+        const params = new URLSearchParams();
+        if (status) params.append("status", status);
+        if (searchParams.get("orderBy"))
+          params.append("orderBy", searchParams.get("orderBy")!);
         // do something with the selected status
-        const query = status ? `?status=${status}` : "";
+        const query = params.size ? "?" + params.toString() : "";
         router.push("/issues/list" + query);
       }}
     >
