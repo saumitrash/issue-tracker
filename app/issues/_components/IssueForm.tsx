97672d9390768eaa24b8afb44594c41830695d8b
saumitrash <darkyed7@gmail.com>
2024-06-21 03:30:12 +0530

    feat: sort issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -39,7 +39,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
         await axios.post("/api/issues", data);
       }
 
-      router.push("/issues");
+      router.push("/issues/list");
       router.refresh();
     } catch (error) {
       setIsSubmitting(false);
