bf8988ec191ca44b7e01de01dfa47bc1806748f5
saumitrash <darkyed7@gmail.com>
2024-06-21 05:36:08 +0530

    feat: build the issue summary component

---
+++ app/IssueSummary.tsx
@@ -0,0 +1,43 @@
+import { Status } from "@prisma/client";
+import { Card, Flex, Text } from "@radix-ui/themes";
+import Link from "next/link";
+import React from "react";
+
+interface Props {
+  open: number;
+  closed: number;
+  inProgress: number;
+}
+
+const IssueSummary = ({ open, closed, inProgress }: Props) => {
+  const container: {
+    label: string;
+    value: number;
+    status: Status;
+  }[] = [
+    { label: "Open Issues", value: open, status: "OPEN" },
+    { label: "In-Progress Issues", value: inProgress, status: "IN_PROGRESS" },
+    { label: "Closed Issues", value: closed, status: "CLOSED" },
+  ];
+  return (
+    <Flex gap="4">
+      {container.map((container) => (
+        <Card key={container.label}>
+          <Flex direction="column" gap="1">
+            <Link
+              className="text-sm font-medium"
+              href={`/issues/list?status=${container.status}`}
+            >
+              {container.label}
+            </Link>
+            <Text size="5" className="font-bold">
+              {container.value}
+            </Text>
+          </Flex>
+        </Card>
+      ))}
+    </Flex>
+  );
+};
+
+export default IssueSummary;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
+import prisma from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
+import { Status } from "@prisma/client";
+import { count } from "console";
 
-export default function Home() {
+export default async function Home() {
+  const counts = await prisma.issue.groupBy({
+    by: ["status"],
+    _count: true,
+  });
+  const countMap = {
+    OPEN: counts.find((c) => c.status === "OPEN")?._count || 0,
+    IN_PROGRESS: counts.find((c) => c.status === "IN_PROGRESS")?._count || 0,
+    CLOSED: counts.find((c) => c.status === "CLOSED")?._count || 0,
+  };
+
   return (
     <>
-      <LatestIssues />
+      <IssueSummary
+        open={countMap.OPEN}
+        closed={countMap.CLOSED}
+        inProgress={countMap.IN_PROGRESS}
+      />
     </>
   );
 }
